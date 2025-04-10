7f1b9345695cb4883f30cde78b66258dcebc56bc
dongdongdongk <dhk9309@gmail.com>
2025-04-10 11:37:20 +0900

    충돌 버그 수정정

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -9,10 +9,12 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] float loadDelay = 1f;
     [SerializeField] ParticleSystem crashEffect; // Reference to the particle system prefab
     [SerializeField] AudioClip crashSFX;
+    bool hsaCrashed = false; // Flag to check if the player has already crashed
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Ground"))
+        if (collision.CompareTag("Ground") && !hsaCrashed)
         {
+            hsaCrashed = true; // Set the flag to true to prevent multiple crashes
             FindObjectOfType<PlayerController>().DisableControls(); // Disable player controls
             GetComponent<AudioSource>().PlayOneShot(crashSFX); // Play the audio source
             crashEffect.Play(); // Play the particle effect
